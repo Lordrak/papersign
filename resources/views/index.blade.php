4eb601be4ff761e3a3a8c87378dfd8ff1bfb8ebd
Mickael LY <ly.mickael@gmail.com>
2018-02-27 15:54:54 +0100

    ma branch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,24 @@
 <html>
 <head>
 	<title>PaperSign</title>
+	
 </head>
 
 <body>
+Yoooo
+
+<h1>
+  Draw over image
+</h1>
+<div class="wrapper">
+  <img src="http://www.licensing.biz/cimages/c0f1a3a97bc0de97271f9813f7715bb3.png" width=400 height=200 />
+  <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
+</div>
+<div>
+  <button id="save">Save</button>
+  <button id="clear">Clear</button>
+</div>
+
 
 </body>
 </html>
