61d16d04bedfcdb9573c510af6f91d6c84857da2
Mickael LY <ly.mickael@gmail.com>
2018-03-05 18:04:05 +0100

    ajout de la signature

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,24 +2,30 @@
 <html>
 <head>
 	<title>PaperSign</title>
-	
 </head>
 
 <body>
-Yoooo
+	Yoooo
+	
+	<p align="center">
+		<canvas id="signature-canvas" style="width:180px;height:80px;max-width:100%;border:6px #CCC solid;background-color: white;"></canvas>
+		</p>
 
-<h1>
-  Draw over image
-</h1>
-<div class="wrapper">
-  <img src="http://www.licensing.biz/cimages/c0f1a3a97bc0de97271f9813f7715bb3.png" width=400 height=200 />
-  <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
-</div>
-<div>
-  <button id="save">Save</button>
-  <button id="clear">Clear</button>
-</div>
+		<div id="signature-message"></div>
 
+		<div id="signature-buttons">
+			<input type="button" id="clear-signature-btn" value="Clear">
+		</div>
+
+		<input type="hidden" name="signature" id="signature-data" value="">
+
+		<br>
+		<div class="signature_pad_save">
+				<button type="submit" class="btn btn-primary" id="save-signature-btn">Save</button>
+		</div>
+
+<script src='https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js'></script>
+<script type="text/javascript" src="{{asset('js/script.js')}}"></script>
 
 </body>
 </html>
